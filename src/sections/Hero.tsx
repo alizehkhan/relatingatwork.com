1decc381f23bf825a66b98ec9c00f69296dd586f
Alizeh Khan <alizeh.khan@staffbase.com>
2026-01-29 20:22:06 +0100

    fix

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,13 +3,18 @@ import { Navigation } from "../components/Navigation";
 
 export const Hero = () => {
   return (
-    <div className="hero min-h-[100svh] bg-cover bg-center bg-no-repeat">
+    <div className="min-h-screen bg-transparent bg-cover bg-center md:bg-[linear-gradient(90deg,rgba(0,0,0,0.6)0%,rgba(0,0,0,0)100%),url('/hero.jpeg')]">
       <Navigation />
-      <div className="container mx-auto mt-20 pb-8">
-        <h1 className="max-w-2xl font-serif text-4xl leading-[1.12] font-bold text-white/90 md:text-7xl md:leading-[1.12]">
+      <div className="container mt-0 pb-8 md:mt-20">
+        <img
+          src="/hero.jpeg"
+          alt=""
+          className="mb-4 block aspect-square rounded-md object-cover md:hidden"
+        />
+        <h1 className="max-w-2xl font-serif text-4xl leading-[1.12] font-bold text-gray-700 md:text-7xl md:text-white/90">
           Bring radically clear communication to your workplace
         </h1>
-        <p className="mt-2 max-w-[360px] text-2xl text-white opacity-80">
+        <p className="mt-2 max-w-100 text-2xl text-gray-600 md:text-white/80">
           The key to greater engagement & efficiency in the workplace
         </p>
         <Button />
